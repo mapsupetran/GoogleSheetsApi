b4f1b351d16016463357c169d1aed6a3c5bddab9
marlons <marlons@campaigntrack.com>
2017-03-21 16:46:38 +0800

    Initial Commit 2.

---
+++ GoogleSheetsApi/Entities/EntityBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleSheetsApi.Entities
+{
+    public abstract class EntityBase<T> : IEntity<T>
+    {
+        public T Id { get; set; }
+    }
+
+    public abstract class StringEntityBase : EntityBase<string> { }
+
+    public abstract class IntegerEntityBase : EntityBase<int> { }
+}
---
+++ GoogleSheetsApi/Program.cs
@@ -0,0 +1,85 @@
+﻿
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SheetsQuickstart
+{
+    class Program
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        static string ApplicationName = "Google Sheets API .NET Quickstart";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                UserCredential credential;
+
+                using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+                {
+                    string credPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+                    credPath = Path.Combine(credPath, ".credentials/sheets.googleapis.com-dotnet-quickstart.json");
+
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.Load(stream).Secrets,
+                        Scopes,
+                        "marlons@campaigntrack.com",
+                        CancellationToken.None,
+                        new FileDataStore(credPath, true)).Result;
+                    Console.WriteLine("Credential file saved to: " + credPath);
+                }
+
+                // Create Google Sheets API service.
+                var service = new SheetsService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName,
+                });
+
+                // Define request parameters.
+                String spreadsheetId = "1dAa5tHdqkq2brQ5pfoFjiv1GkVxge52bn99Oz2AgrQw";
+                String range = "Data!A2:D";
+                SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+                // Prints the names and majors of students in a sample spreadsheet:
+                // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+                ValueRange response = request.Execute();
+                IList<IList<Object>> values = response.Values;
+                if (values != null && values.Any())
+                {
+                    Console.WriteLine("Name, Major");
+                    foreach (var row in values)
+                    {
+                        // Print columns A and E, which correspond to indices 0 and 4.
+                        Console.WriteLine("ID = {0}, Name = {1}", row[0], row[2]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No data found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            finally
+            {
+                Console.Read(); 
+            }
+        }
+    }
+}
